5bee0a56d84c92017ab3dc1f2caadb0ce4118f5f
RFIDGMPS <kyebejeanu@gmail.com>
2024-10-16 13:29:17 +0800

    s

--- main.php
+++ main.php
@@ -531,42 +531,52 @@ if ($row) {
 
         // Combine and fetch data from both tables for the current date, ordering by the latest update
         $results = mysqli_query($db, "
-        SELECT 
-        p.photo,
-        p.department,
-        p.role,
-        CONCAT(p.first_name, ' ', p.last_name) AS full_name,
-        CASE
-            WHEN CURRENT_TIME() < '12:00:00' THEN pl.time_in_am
-            ELSE pl.time_in_pm
-        END AS time_in,
-        CASE
-            WHEN CURRENT_TIME() < '12:00:00' THEN pl.time_out_am
-            ELSE pl.time_out_pm
-        END AS time_out,
-        pl.date_logged,
-        pl.id -- Assuming id is the primary key and auto-increments
-    FROM personell_logs pl
-    JOIN personell p ON pl.personnel_id = p.id
-    WHERE pl.date_logged = CURRENT_DATE()
-    
-    UNION
-    
+      WITH current_time AS (
     SELECT 
-        vl.photo,
-        vl.department,
-        'Visitor' AS role,
-        vl.name AS full_name,
-        vl.time_in,
-        vl.time_out,
-        vl.date_logged,
-        vl.id -- Assuming id is the primary key in visitor_logs
-    FROM visitor_logs vl
-    WHERE vl.date_logged = CURRENT_DATE()
-    
-    ORDER BY 
-        id DESC -- Sorting by the most recent id
-    LIMIT 1;
+        CASE 
+            WHEN CURRENT_TIME() < '12:00:00' THEN 'AM'
+            ELSE 'PM'
+        END AS period
+)
+
+SELECT 
+    p.photo,
+    p.department,
+    p.role,
+    CONCAT(p.first_name, ' ', p.last_name) AS full_name,
+    CASE
+        WHEN ct.period = 'AM' THEN pl.time_in_am
+        ELSE pl.time_in_pm
+    END AS time_in,
+    CASE
+        WHEN ct.period = 'AM' THEN pl.time_out_am
+        ELSE pl.time_out_pm
+    END AS time_out,
+    pl.date_logged,
+    pl.id
+FROM personell_logs pl
+JOIN personell p ON pl.personnel_id = p.id
+JOIN current_time ct ON 1=1
+WHERE pl.date_logged = CURRENT_DATE()
+
+UNION
+
+SELECT 
+    vl.photo,
+    vl.department,
+    'Visitor' AS role,
+    vl.name AS full_name,
+    vl.time_in,
+    vl.time_out,
+    vl.date_logged,
+    vl.id
+FROM visitor_logs vl
+WHERE vl.date_logged = CURRENT_DATE()
+
+ORDER BY 
+    id DESC
+;
+
     
 
     ");
