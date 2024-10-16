fbe888449bbeef9e8a52519e8973fefa5a4af884
RFIDGMPS <kyebejeanu@gmail.com>
2024-10-16 16:03:45 +0800

    s

--- test.php
+++ test.php
@@ -1,11 +1,11 @@
 <?php
 include 'connection.php';  // Ensure this file contains the DB connection logic
 
-$sql = "TRUNCATE TABLE personell_logs;";
+$sql = "TRUNCATE TABLE room_logs;";
 
 // Execute the query
 if ($db->query($sql) === TRUE) {
-    echo "Table 'personell_logs' truncated successfully.";
+    echo "Table 'room_logs' truncated successfully.";
 } else {
     echo "Error truncating table: " . $db->error;
 }
