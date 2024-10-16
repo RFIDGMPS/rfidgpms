dd10b6ece92a622c739440e0a51ae798eab081e9
RFIDGMPS <kyebejeanu@gmail.com>
2024-10-16 15:19:46 +0800

    s

--- test.php
+++ test.php
@@ -3,7 +3,7 @@ include 'connection.php';  // Ensure this file contains the DB connection logic
 
 
 // SQL query to select all records from room_logs
-$sql = "SELECT id, personnel_id, time_in, date_logged, location FROM room_logs";
+$sql = "SELECT id, personnel_id, time_in,time_out, date_logged, location FROM room_logs";
 $result = $db->query($sql);
 
 // Check if there are results and display them
@@ -24,6 +24,7 @@ if ($result->num_rows > 0) {
                 <td>" . htmlspecialchars($row['id']) . "</td>
                 <td>" . htmlspecialchars($row['personnel_id']) . "</td>
                 <td>" . htmlspecialchars($row['time_in']) . "</td>
+                   <td>" . htmlspecialchars($row['time_out']) . "</td>
                 <td>" . htmlspecialchars($row['date_logged']) . "</td>
                 <td>" . htmlspecialchars($row['location']) . "</td>
               </tr>";
@@ -37,4 +38,4 @@ if ($result->num_rows > 0) {
 $db->close();
 ?>
 
-?>
+
