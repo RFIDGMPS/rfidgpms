9105f78cd0b3d55cb048b35853bcd28151256c4d
RFIDGMPS <kyebejeanu@gmail.com>
2024-10-20 15:09:37 +0800

    s

--- admin/room.php
+++ admin/room.php
@@ -72,7 +72,7 @@ include 'header.php';
 
 
 
-                                 <?php $results = mysqli_query($db, "SELECT * FROM rooms"); ?>
+                                 <?php $results = mysqli_query($db, "SELECT * FROM rooms ORDER BY id"); ?>
                                  <?php while ($row = mysqli_fetch_array($results)) { ?>
                                     <tr  class="table-<?php echo $row['id'];?>">
                                   
