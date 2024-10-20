fa207d0ffab9effa35f9667d220e35f9bfff5868
RFIDGMPS <kyebejeanu@gmail.com>
2024-10-20 13:15:52 +0800

    s

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -153,7 +153,7 @@ $strangers = getCount($db, "SELECT COUNT(*) AS count FROM stranger_logs WHERE la
                                 <h6 class="mb-0"><?php echo $blocked; ?></h6>
                             </div>
                         </div>
-                        <div id="blockLogs" class="stranger-logs" style="display:none; position: absolute;background: white; border: 1px solid #ccc; border-radius: 5px; z-index: 100;box-shadow: 0 2px 10px rgba(0,0,0,0.1); max-height: 200px;">
+                        <div id="blockLogs" class="stranger-logs" style="display:none; position: absolute;background: white; border: 1px solid #ccc; padding: 10px;border-radius: 5px; z-index: 100;box-shadow: 0 2px 10px rgba(0,0,0,0.1); max-height: 200px;">
        
         <ul class="list-unstyled">
             <?php
