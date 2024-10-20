cfebdc450a89e9441f063a3047d3056d9ad9fa79
RFIDGMPS <kyebejeanu@gmail.com>
2024-10-20 13:31:16 +0800

    s

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -165,9 +165,9 @@ $strangers = getCount($db, "SELECT COUNT(*) AS count FROM stranger_logs WHERE la
             $result = $db->query($sql);
             if ($result->num_rows > 0) {
                 while ($row = $result->fetch_assoc()) {
-                   echo '<li class="mb-2 d-flex align-items-left">';
+                    echo '<li class="mb-2 d-flex align-items-center">';
                     echo '<span><img style="border-radius:50%;" src="uploads/' . htmlspecialchars($row["photo"]) . '" width="50px" height="50px"/></span>';
-                    echo '<span class="text-muted ms-3" style="vertical-align:middle;"><b>' . htmlspecialchars($row["full_name"]) . '</b></span>';
+                    echo '<span class="text-muted ms-3"><b>' . htmlspecialchars($row["full_name"]) . '</b></span>';
                     echo '</li>';
                 }
             } else {
