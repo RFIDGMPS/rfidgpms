719a03b631eab6d17ee367cc351a6d09228fed51
RFIDGMPS <kyebejeanu@gmail.com>
2024-11-20 00:05:27 +0800

    s

--- admin/settings.php
+++ admin/settings.php
@@ -4,9 +4,9 @@ include 'auth.php'; // Include session validation
 <?php
 include '../connection.php';
 $logo1 = "";
-$name = "";
-$address = "";
-$logo2 = "";
+    $name = "";
+    $address = "";
+    $logo2 = "";
 // Fetch data from the about table
 $sql = "SELECT * FROM about LIMIT 1";
 $result = $db->query($sql);
@@ -26,7 +26,7 @@ if ($result->num_rows > 0) {
 <?php include 'header.php'; ?>
 
 <body>
-    <div class="container-fluid position-relative bg-light p-0">
+    <div class="container-fluid position-relative bg-white d-flex p-0">
         <?php include 'sidebar.php'; ?>
         
         <div class="content">
@@ -36,66 +36,82 @@ if ($result->num_rows > 0) {
 
             <div class="container-fluid pt-4 px-4">
                 <div class="col-sm-12 col-xl-12">
-                    <div class="card shadow-sm rounded-lg">
-                        <div class="card-header d-flex justify-content-between align-items-center">
-                            <h6 class="mb-0">About</h6>
+                    <div class="bg-light rounded p-4">
+                        <div class="row">
+                            <div class="col-9">
+                                <h6 class="mb-4">About</h6>
+                            </div>
+                            
+                            <div class="col-lg-3 mt-4" style="text-align:right;">
+                              
                             <button id="editButton" class="btn btn-primary" onclick="toggleEdit()"><i class="bi bi-pencil"></i> Edit</button>
+            
+                            
+                            </div>
                         </div>
-                        <div class="card-body">
-                            <form role="form" method="post" action="edit1.php?edit=about" enctype="multipart/form-data">
-                                <div class="row mb-3">
-                                    <div class="col-md-6">
-                                        <label for="name" class="form-label">Name:</label>
-                                        <input type="text" id="name" class="form-control" name="name" value="<?php echo $name; ?>" autocomplete="off" readonly />
-                                    </div>
-                                    <div class="col-md-6">
-                                        <label for="address" class="form-label">Address:</label>
-                                        <input type="text" id="address" class="form-control" name="address" value="<?php echo $address; ?>" autocomplete="off" readonly />
-                                    </div>
+                        <br>
+                        <form role="form" method="post" action="edit1.php?edit=about" enctype="multipart/form-data">
+                            <div class="row">
+                                <div class="col">
+                                    <label>Name:</label>
+                                    <input type="text" class="form-control" name="name" value="<?php echo $name; ?>" autocomplete="off" readonly />
                                 </div>
-                                <div class="row mb-3">
-                                    <div class="col-md-6">
-                                        <label for="logo1" class="form-label">Logo 1:</label>
-                                        <div class="file-uploader">
-                                            <label class="upload-img-btn">
-                                                <input type="file" id="logo1" name="logo1" class="upload-field" style="display:none;" accept="image/*" title="Upload Logo 1" disabled readonly />
-                                                <input type="hidden" name="logo1" class="logo1">
-                                                <img class="preview-1 edit-logo1" src="<?php echo 'uploads/'.$logo1; ?>" class="img-fluid" style="height: 130px; width: 140px; border: 1px solid #ccc;" />
-                                            </label>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <label for="logo2" class="form-label">Logo 2:</label>
-                                        <div class="file-uploader">
-                                            <label class="upload-img-btn">
-                                                <input type="file" id="logo2" name="logo2" class="upload-field" style="display:none;" accept="image/*" title="Upload Logo 2" disabled readonly />
-                                                <input type="hidden" name="logo2" class="logo2">
-                                                <img class="preview-2 edit-logo2" src="<?php echo 'uploads/'.$logo2; ?>" class="img-fluid" style="height: 130px; width: 140px; border: 1px solid #ccc;" />
-                                            </label>
-                                        </div>
-                                    </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <label>Address:</label>
+                                    <input type="text" class="form-control" name="address" value="<?php echo $address; ?>" autocomplete="off" readonly />
                                 </div>
-                                <div class="d-flex justify-content-end">
-                                    <button type="submit" class="btn btn-success" id="saveButton" style="padding: 10px 20px; display:none;"> <i class="bi bi-save"></i> Save</button> 
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                <label>Logo 1:</label><br/>
+                                <div class="file-uploader">
+               <label name="upload-label" class="upload-img-btn">
+               <input type="file" id="photo" name="logo1" class="upload-field-1" style="display:none;" accept="image/*" title="Upload Foto.." disabled readonly/>
+               <input type="hidden" id="logo1" name="logo1" class="logo1">
+               <img class="preview-1 edit-logo1" src="<?php echo 'uploads/'.$logo1; ?>" style="width: 140px!important;height: 130px!important;border: 1px solid gray;top: 20%;" title="Upload Photo.." />
+               </label>
+            </div>
                                 </div>
-                            </form>
-                        </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <label>Logo 2:</label><br/>
+                                    <div class="file-uploader">
+               <label name="upload-label" class="upload-img-btn">
+               <input type="file" id="photo" name="logo2" class="upload-field-2" style="display:none;" accept="image/*" title="Upload Foto.." disabled readonly/>
+               <input type="hidden" id="logo2" name="logo2" class="logo2">
+               <img class="preview-2 edit-logo2" src="<?php echo 'uploads/'.$logo2; ?>" style="width: 140px!important;height: 130px!important;border: 1px solid gray;top: 15%;" title="Upload Photo.." />
+               </label>
+            </div>
+                                </div>
+                            </div>
+                            <div class="col" style="text-align:right; ">
+                               
+                                <button type="submit" class="btn btn-success" id="btn_print" style="padding:20px;"> <i class="bi bi-save"></i> Save</button> 
+            
+                            
+                            </div>
+                        </form>
                     </div>
+                    <hr>
                 </div>
             </div>
-
             <?php include 'footer.php'; ?>
         </div>
-    </div>
-
-    <script>
-        $(document).ready(function() {
+        <script type="text/javascript">
+         $(document).ready(function() {
             $logo1 =  $('.edit-logo1').attr('src');
             $logo2 =  $('.edit-logo2').attr('src');
-            $('.logo1').val($logo1);
-            $('.logo2').val($logo2);
-        });
+           
+$('.logo1').val($logo1);
+$('.logo2').val($logo2);
+         });
+		 
+		 </script>
 
+<script>
         function toggleEdit() {
             const inputs = document.querySelectorAll('input[type="text"], input[type="file"]');
             const saveButton = document.getElementById('saveButton');
@@ -108,28 +124,28 @@ if ($result->num_rows > 0) {
                     input.setAttribute('disabled', 'disabled');
                 }
             });
-            saveButton.style.display = saveButton.style.display === 'none' ? 'inline-block' : 'none';
+            saveButton.disabled = !saveButton.disabled;
         }
     </script>
 
-    <script type="text/javascript">
-        function readURL(input) {
-            if (input.files && input.files[0]) {
-                var reader = new FileReader();
-                reader.onload = function(e) {
-                    var num = $(input).attr('class').split('-')[2];
-                    $('.file-uploader .preview-' + num).attr('src', e.target.result);
-                }
-                reader.readAsDataURL(input.files[0]);
+
+        <script type="text/javascript">
+            function readURL(input) {
+            	if (input.files && input.files[0]) {
+            		var reader = new FileReader();
+            		reader.onload = function(e) {
+            			var num = $(input).attr('class').split('-')[2];
+            			$('.file-uploader .preview-' + num).attr('src', e.target.result);
+            		}
+            		reader.readAsDataURL(input.files[0]);
+            	}
             }
-        }
-        $("[class^=upload-field-]").change(function() {
-            readURL(this);
-        });
-    </script>
-
-    <a href="#" class="btn btn-lg btn-warning btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
-
+            $("[class^=upload-field-]").change(function() {
+            	readURL(this);
+            });
+         </script>
+        <a href="#" class="btn btn-lg btn-warning btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="lib/chart/chart.min.js"></script>
     <script src="lib/easing/easing.min.js"></script>
