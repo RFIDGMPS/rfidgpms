1ff238ce38ca4e810be50254441b1fc169b5cb67
RFIDGMPS <kyebejeanu@gmail.com>
2024-11-19 11:08:31 +0800

    s

--- admin/transac.php
+++ admin/transac.php
@@ -11,37 +11,58 @@
 switch ($_GET['action'])
 {
     case 'add':
-       
-        $id_no = $_POST['id_no'];
-        $rfid_number = $_POST['rfid_number'];
-        $last_name = $_POST['last_name'];
-        $first_name = $_POST['first_name'];
-        $middle_name = $_POST['middle_name'];
-        $date_of_birth = $_POST['date_of_birth'];
-        $place_of_birth = $_POST['place_of_birth'];
-        $role = $_POST['role'];
-        $sex = $_POST['sex'];
-        $civil_status = $_POST['stat'];
-        $contact_number = $_POST['contact_number'];
-        $email_address = $_POST['email_address'];
-        $department = $_POST['department'];
-        
-        $section = $_POST['section'];
-        $status = $_POST['status'];
-        $category = $_POST['category'];
-        $complete_address = $_POST['complete_address'];
-        $photo = $_FILES['photo']['name'];
-       
-        $target_dir = "uploads/";
-        $target_file = $target_dir . basename($_FILES["photo"]["name"]);
-        move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file);
-               $query = "INSERT INTO personell (id_no,category, rfid_number, last_name, first_name, middle_name, date_of_birth, role, sex, civil_status, contact_number, email_address, department, section, status, complete_address, photo, place_of_birth)
-               VALUES ('$id_no', '$category','$rfid_number', '$last_name', '$first_name', '$middle_name', '$date_of_birth', '$role', '$sex', '$civil_status', '$contact_number', '$email_address', '$department', '$section', '$status', '$complete_address', '$photo', '$place_of_birth')";
-               mysqli_query($db, $query) or die('Error in updating Database');
-               echo '<script type="text/javascript">
-               alert("Successfully added.");
-               window.location = "personell.php";
-       </script>';
+      
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Collect form data
+    $id_no = $_POST['id_no'];
+    $rfid_number = $_POST['rfid_number'];
+    $last_name = $_POST['last_name'];
+    $first_name = $_POST['first_name'];
+    $middle_name = $_POST['middle_name'];
+    $date_of_birth = $_POST['date_of_birth'];
+    $place_of_birth = $_POST['place_of_birth'];
+    $role = $_POST['role'];
+    $sex = $_POST['sex'];
+    $civil_status = $_POST['stat'];
+    $contact_number = $_POST['contact_number'];
+    $email_address = $_POST['email_address'];
+    $department = $_POST['department'];
+    $section = $_POST['section'];
+    $status = $_POST['status'];
+    $category = $_POST['category'];
+    $complete_address = $_POST['complete_address'];
+    $photo = $_FILES['photo']['name'];
+
+    // Move uploaded photo to the target directory
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["photo"]["name"]);
+    move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file);
+
+    // Insert data into database
+    $query = "INSERT INTO personell (id_no, category, rfid_number, last_name, first_name, middle_name, date_of_birth, role, sex, civil_status, contact_number, email_address, department, section, status, complete_address, photo, place_of_birth) 
+              VALUES ('$id_no', '$category', '$rfid_number', '$last_name', '$first_name', '$middle_name', '$date_of_birth', '$role', '$sex', '$civil_status', '$contact_number', '$email_address', '$department', '$section', '$status', '$complete_address', '$photo', '$place_of_birth')";
+    if (mysqli_query($db, $query)) {
+        echo '<script type="text/javascript">
+                Swal.fire({
+                    icon: "success",
+                    title: "Successfully Added!",
+                    text: "New user has been added.",
+                    confirmButtonText: "OK"
+                }).then(function() {
+                    window.location = "personell.php";
+                });
+              </script>';
+    } else {
+        echo '<script type="text/javascript">
+                Swal.fire({
+                    icon: "error",
+                    title: "Error",
+                    text: "There was an issue adding the user.",
+                    confirmButtonText: "Try Again"
+                });
+              </script>';
+    }
+}
     break;
     case 'add_department':
 // Get the POST data
