2085dbaa6cf6808846cf2f0219728d2d84b928ef
RFIDGMPS <kyebejeanu@gmail.com>
2024-11-21 10:42:19 +0800

    s

--- test.php
+++ test.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php
-include 'header.php';
-?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Search Personnel</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
 <body>
     <h2>Search Personnel</h2>
     <input type="text" id="search-box" placeholder="Type a name..." />
