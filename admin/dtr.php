4cfae55b12b6195c4f79cd05bc32429c969f09a0
RFIDGMPS <kyebejeanu@gmail.com>
2024-11-21 11:54:12 +0800

    d

--- admin/dtr.php
+++ admin/dtr.php
@@ -170,6 +170,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['query'])) {
                     console.error('Error fetching data:', error);
                 });
         });
+
+        alert(searchInput.value);
     </script>
                
             
