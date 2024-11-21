3996f8dd9f274dbcccc88a6e640e0564fb5930df
RFIDGMPS <kyebejeanu@gmail.com>
2024-11-21 15:38:36 +0800

    d

--- admin/dtr.php
+++ admin/dtr.php
@@ -347,7 +347,7 @@ for ($day = 1; $day <= 31; $day++) {
 
     // Prepare and execute the query
     $stmt = $db->prepare($sql);
-    $stmt->bind_param("si", $month, $personnel_id); // Bind parameters
+    $stmt->bind_param("si", $month, $id); // Bind parameters
     $stmt->execute();
     $result = $stmt->get_result();
 
