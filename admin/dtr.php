ad4e5764916f8a9d9cabf9acceb2f14b62b1ea1b
RFIDGMPS <kyebejeanu@gmail.com>
2024-11-21 11:24:40 +0800

    s

--- admin/dtr.php
+++ admin/dtr.php
@@ -396,31 +396,31 @@ function convertTo12Hour($time) {
         $('#date1').datepicker();
         $('#date2').datepicker();
         
-        $('#btn_search').on('click', function() {
-            if ($('#date1').val() == "" || $('#date2').val() == "") {
-                alert("Please enter Date 'From' and 'To' before submit");
-            } else {
-                $date1 = $('#date1').val();
-                $date2 = $('#date2').val();
-                $('#load_data').empty();
-                $loader = $('<tr ><td colspan = "6"><center>Searching....</center></td></tr>');
-                $loader.appendTo('#load_data');
-                setTimeout(function() {
-                    $loader.remove();
-                    $.ajax({
-                        url: '../config/init/report_attendance.php',
-                        type: 'POST',
-                        data: {
-                            date1: $date1,
-                            date2: $date2
-                        },
-                        success: function(res) {
-                            $('#load_data').html(res);
-                        }
-                    });
-                }, 1000);
-            }
-        });
+        // $('#btn_search').on('click', function() {
+        //     if ($('#date1').val() == "" || $('#date2').val() == "") {
+        //         alert("Please enter Date 'From' and 'To' before submit");
+        //     } else {
+        //         $date1 = $('#date1').val();
+        //         $date2 = $('#date2').val();
+        //         $('#load_data').empty();
+        //         $loader = $('<tr ><td colspan = "6"><center>Searching....</center></td></tr>');
+        //         $loader.appendTo('#load_data');
+        //         setTimeout(function() {
+        //             $loader.remove();
+        //             $.ajax({
+        //                 url: '../config/init/report_attendance.php',
+        //                 type: 'POST',
+        //                 data: {
+        //                     date1: $date1,
+        //                     date2: $date2
+        //                 },
+        //                 success: function(res) {
+        //                     $('#load_data').html(res);
+        //                 }
+        //             });
+        //         }, 1000);
+        //     }
+        // });
 
         $('#reset').on('click', function() {
             location.reload();
