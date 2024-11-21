da26111b379b2a595d987fd1b5a0432cb00c8a6b
RFIDGMPS <kyebejeanu@gmail.com>
2024-11-21 11:01:35 +0800

    s

--- admin/dtr.php
+++ admin/dtr.php
@@ -90,7 +90,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['query'])) {
             overflow-y: auto;
             background-color: white; /* Make background white to distinguish from page */
             width: 200px; /* Adjust width as needed */
-            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); /* Add shadow for better visibility */
+            /* box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); Add shadow for better visibility */
             margin-top: 5px; /* Slight margin below the input box */
         }
 
