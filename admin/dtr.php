04b2b62d82b238531feb79d78feb58a91b71e53e
RFIDGMPS <kyebejeanu@gmail.com>
2024-11-26 14:11:50 +0800

    x

--- admin/dtr.php
+++ admin/dtr.php
@@ -362,25 +362,25 @@ echo $timeData['time_out_am'];
 echo $timeData['time_in_pm'];
 echo $timeData['time_out_pm'];
     // Set default values if fields are '?' (which means they were originally null)
-    if ($timeData['time_in_am'] != '?' || $timeData['time_in_am'] != null) {
+    if ($timeData['time_in_am'] != '?' && $timeData['time_in_am'] != null) {
         $timeData['time_in_am'] = '08:00 AM';
     }
     else{
         $timeData['time_in_am'] = '?';
     }
-    if ($timeData['time_out_am'] != '?' || $timeData['time_out_am'] != null) {
+    if ($timeData['time_out_am'] != '?' && $timeData['time_out_am'] != null) {
         $timeData['time_out_am'] = '12:00 PM';
     }
     else{
         $timeData['time_in_am'] = '?';
     }
-    if ($timeData['time_in_pm'] != '?' || $timeData['time_in_pm'] != null) {
+    if ($timeData['time_in_pm'] != '?' && $timeData['time_in_pm'] != null) {
         $timeData['time_in_pm'] = '01:00 PM';
     }
     else{
         $timeData['time_in_am'] = '?';
     }
-    if ($timeData['time_out_pm'] != '?' || $timeData['time_out_pm'] != null) {
+    if ($timeData['time_out_pm'] != '?' && $timeData['time_out_pm'] != null) {
         $timeData['time_out_pm'] = '05:00 PM';
     }else{
         $timeData['time_in_am'] = '?';
