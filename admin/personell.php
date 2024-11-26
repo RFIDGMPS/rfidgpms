c43589bbbd123887c7fe2949a8ce7edfbe9d31ac
RFIDGMPS <kyebejeanu@gmail.com>
2024-11-26 16:05:41 +0800

    k

--- admin/personell.php
+++ admin/personell.php
@@ -61,8 +61,8 @@ include '../connection.php';
                                     <th scope="col">Department</th>
                                     <th scope="col">Status</th>
                                     <th scope="col">Action</th>
-                                    <th style="visibility: collapse;">Date Added</th> <!-- Hidden header for the date_added column -->
-      
+                                    <!-- <th style="display: none;">Date Added</th> 
+       -->
                                  </tr>
                               </thead>
                               <tbody>
@@ -119,8 +119,8 @@ include '../connection.php';
 
                                        </center>
                                     </td>
-                                    <td style="visibility: collapse;" class="hidden-date"><?php echo $row['date_added']; ?></td> <!-- Hidden column -->
-         
+                                    <!-- <td style="display:none;" class="hidden-date"><?php //echo $row['date_added']; ?></td> -->
+          -->
                                  </tr>
                                  <?php } ?>
                               </tbody>
