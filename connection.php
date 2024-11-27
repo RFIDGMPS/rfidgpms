4cb2cd083fe2ebf70be0743c612c20e2dd9436dc
RFIDGMPS <kyebejeanu@gmail.com>
2024-11-27 08:32:21 +0800

    s

--- connection.php
+++ connection.php
@@ -1,4 +1,5 @@
  <?php
+ date_default_timezone_set('Asia/Manila');
  $db = mysqli_connect('127.0.0.1', 'u510162695_gatepassdb', '1Gatepassdb') or
         die ('Unable to connect. Check your connection parameters.');
         mysqli_select_db($db, 'u510162695_gatepassdb' ) or die(mysqli_error($db));
