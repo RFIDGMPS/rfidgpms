616f7957fa9c70c8421343e9a662fef02de4b7f8
RFIDGMPS <kyebejeanu@gmail.com>
2024-11-27 17:51:19 +0800

    x

--- admin/logout.php
+++ admin/logout.php
@@ -8,6 +8,6 @@ session_unset();
 session_destroy();
 
 // Redirect to login page
-header("Location: index.php");
+header("Location: index");
 exit();
 ?>
--- admin/lostcard.php
+++ admin/lostcard.php
@@ -144,7 +144,7 @@ include 'header.php';
                     xhr.onload = function () {
                         if (xhr.status === 200) {
                             Swal.fire('Blocked!', 'The user has been blocked.', 'success').then(() => {
-                                window.location.href = 'lostcard.php';
+                                window.location.href = 'lostcard';
                             });
                         } else {
                             Swal.fire('Error', 'There was an error blocking the user.', 'error');
@@ -173,7 +173,7 @@ include 'header.php';
                     xhr.onload = function () {
                         if (xhr.status === 200) {
                             Swal.fire('Deleted!', 'The request has been deleted.', 'success').then(() => {
-                                window.location.href = 'lostcard.php';
+                                window.location.href = 'lostcard';
                             });
                         } else {
                             Swal.fire('Error', 'There was an error deleting the request.', 'error');
