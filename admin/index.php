425d6a65014a19406e41cd3c12b6a1d931ca46dd
RFIDGMPS <kyebejeanu@gmail.com>
2024-11-28 14:01:56 +0800

    x

--- admin/index.php
+++ admin/index.php
@@ -297,7 +297,7 @@ echo "<script>var lockout = { attempts: " . $_SESSION['login_attempts'] . ", rem
                        
                     </form>
                     <a class="terms-link" href="../terms">Terms and Conditions</a>
-                    <a class="terms-link" href="../plan">Incident Response</a>
+                    <!-- <a class="terms-link" href="../plan">Incident Response</a> -->
 <style>
 .terms-link {
     padding-left: 55%;
