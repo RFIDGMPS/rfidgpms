33be4b3ac3b22f0d3daa972d273d10e9c42c3ee0
RFIDGMPS <kyebejeanu@gmail.com>
2024-11-28 12:39:16 +0800

    z

--- main.php
+++ main.php
@@ -1228,12 +1228,6 @@ Webcam.snap(function(data_uri){
                         <!-- Search Box -->
                         <div id="search" class="form-floating mb-4">
                             <input type="text" class="form-control" id="searchBox" name="pname" placeholder="Search Name" autocomplete="off" onkeyup="searchPersonell(this.value)">
-                            <input type="text" id="searchBox1" placeholder="Search Name" />
-<script>
-    document.getElementById("searchBox1").addEventListener("keyup", function () {
-        alert("Keyup event fired with value:", this.value);
-    });
-</script>
                             <label for="floatingPassword">Search Name</label>
                         </div>
                         <!-- Card to display selected personnel -->
