47e5e850f910ea0ea0a16c89c71caab43d39af61
RFIDGMPS <kyebejeanu@gmail.com>
2024-11-29 16:56:24 +0800

    m

--- session.php
+++ session.php
@@ -87,7 +87,7 @@ $db->close();
 
 function logSession($db, $user_id, $ip_address, $device_fingerprint) {
     $location = fetchLocation($ip_address);
-    $query = "INSERT INTO admin_sessions (location, ip_address, device, date_logged) VALUES (?, ?, ?, ?, NOW())";
+    $query = "INSERT INTO admin_sessions (location, ip_address, device, date_logged) VALUES (?, ?, ?, NOW())";
     $stmt = $db->prepare($query);
     $stmt->bind_param('sss', $location, $ip_address, $device_fingerprint);
     $stmt->execute();
