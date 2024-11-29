af8d5029938de90417a9b0f9b5883e68cb510770
RFIDGMPS <kyebejeanu@gmail.com>
2024-11-29 19:48:42 +0800

    c

--- admin/new_device.php
+++ admin/new_device.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Form</title>
+    <title>Verify Identity</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
     <style>
         body {
@@ -113,7 +113,7 @@
 <body>
 
 <div class="container">
-    <h2>Login to Your Account</h2>
+    <h2>Unrecognized Device is Detected. Confirm Identity</h2>
     <form method="POST" action="session.php">
         <input type="email" name="email" placeholder="Email" required>
 
