3e384b6d7d5c3ab78c7917c0312f854564a8ca0a
RFIDGMPS <kyebejeanu@gmail.com>
2024-11-29 17:26:31 +0800

    b

--- test.php
+++ test.php
@@ -4,8 +4,8 @@
     <input type="password" name="password" placeholder="Password" required>
 
     <label>Choose verification method:</label><br>
-    <input type="radio" name="verification_method" value="email" required> Email<br>
-    <input type="radio" name="verification_method" value="contact" required> Contact Number<br>
+    <input type="radio" name="verification_method" value="link"> Verification Link<br>
+    <input type="radio" name="verification_method" value="otp"> Numeric OTP<br>
 
     <img src="captcha.php" alt="CAPTCHA">
     <input type="text" name="captcha" placeholder="Enter CAPTCHA" required>
