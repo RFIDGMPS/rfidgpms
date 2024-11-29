d9fac1c0223577809a214f1385a4508e2580891c
RFIDGMPS <kyebejeanu@gmail.com>
2024-11-29 22:08:00 +0800

    d

--- admin/verification.php
+++ admin/verification.php
@@ -113,7 +113,7 @@
 <body>
 
 <div class="container">
-    <h2>Unrecognized device has been detected. <br/>Please confirm your identity</h2>
+    <h2>Please confirm your identity</h2>
     <form method="POST" action="session.php">
         <input type="email" name="email" placeholder="Email" required>
 
