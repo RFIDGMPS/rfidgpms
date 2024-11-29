1c4a7d4753b33daa90cbec09dff48e9e48107e90
RFIDGMPS <kyebejeanu@gmail.com>
2024-11-29 16:38:49 +0800

    x

--- session.php
+++ session.php
@@ -1,6 +1,8 @@
 
 <?php
 include 'connection.php';
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 //require 'vendor/autoload.php'; // PHP Mailer autoloader
 session_start();
 require 'admin/PHPMailer/src/Exception.php';
