41afd981141120cbe20e74ea83c893b5ba1c9d02
RFIDGMPS <kyebejeanu@gmail.com>
2024-12-07 10:52:18 +0800

    z

--- admin/auth.php
+++ admin/auth.php
@@ -8,8 +8,19 @@ $globalToken = file_get_contents('global_admin_token.txt');
 // Check if the session token matches the global token
 if (!isset($_SESSION['username']) || !isset($_SESSION['admin_token']) || $_SESSION['admin_token'] !== $globalToken) {
     // Token mismatch; logout the session
+
     session_destroy();
-    header('Location: index');
+    echo "
+    <script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>
+    <script>
+        Swal.fire({
+            icon: 'warning',
+            title: 'Session Terminated',
+            text: 'Your account has been logged out because it was accessed from another device. Please protect your account.'
+        }).then(() => {
+            window.location.href = 'index'; // Redirect to the login page
+        });
+    </script>";
     exit();
 }
 ?>
