dc82f103c25511089af0d4513a9fa752b01b4be4
RFIDGMPS <kyebejeanu@gmail.com>
2024-12-11 17:04:21 +0800

    b

--- admin/header.php
+++ admin/header.php
@@ -26,7 +26,7 @@
       <!--     <link href="img/favicon.ico" rel="icon"> -->
       <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
       <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-      <!-- <script src="https://www.google.com/recaptcha/api.js?render=6LefppQqAAAAAKunsfzmruPzJe8KcazwN5CtLakp"></script> -->
+      <script src="https://www.google.com/recaptcha/api.js?render=6LefppQqAAAAAKunsfzmruPzJe8KcazwN5CtLakp"></script>
 
       <link rel="preconnect" href="https://fonts.googleapis.com">
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
