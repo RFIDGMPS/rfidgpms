f728b789b9425cf5d88289d5db3af5d78c384989
RFIDGMPS <kyebejeanu@gmail.com>
2024-12-19 10:00:46 +0800

    s

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -379,7 +379,37 @@ function hideEntrantsLogs() {
 </div>
 
 
+<div style="padding:20px; margin:10px;width:47%;" class="bg-light rounded">
+<div id="myChart2" style="width:100%; max-width:600px; height:500px;"></div>
 
+<script>
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+
+// Set Data
+const data = google.visualization.arrayToDataTable([
+  ['Contry', 'Mhl'],
+  ['Italy',55],
+  ['France',49],
+  ['Spain',44],
+  ['USA',24],
+  ['Argentina',15]
+]);
+
+// Set Options
+const options = {
+  title:'World Wide Wine Production'
+};
+
+// Draw
+const chart = new google.visualization.BarChart(document.getElementById('myChart2'));
+chart.draw(data, options);
+
+}
+</script>
+</div>
         
         
 </div>
