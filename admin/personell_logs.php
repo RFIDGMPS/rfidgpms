32ac66def0c568fa31a75115750477679b709a56
RFIDGMPS <kyebejeanu@gmail.com>
2024-12-19 12:36:23 +0800

    s

--- admin/personell_logs.php
+++ admin/personell_logs.php
@@ -252,8 +252,7 @@ mysqli_close($db);
     //     // Close database connection
     //     mysqli_close($db);
     // } 
-    if (
-        isset($_POST['date1']) || isset($_POST['date2']) && $_POST['date1'] =="" || $_POST['date2'] =="") {
+    if ( $_POST['date1'] !="" && $_POST['date2'] =="" ||  $_POST['date1'] =="" && $_POST['date2'] !="") {
 echo '<script>alert("Please enter both dates.");</script>';
         }
         else if (
