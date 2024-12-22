75f034d8837cb062d90d195e322c35aa4aae3413
RFIDGMPS <kyebejeanu@gmail.com>
2024-12-22 23:15:12 +0800

    s

--- admin/header.php
+++ admin/header.php
@@ -18,7 +18,7 @@
 <head>
   
       <meta charset="utf-8">
-      <title>Administrator</title>
+      <title>RFIDGPMS</title>
       <meta content="width=device-width, initial-scale=1.0" name="viewport">
       <meta content="" name="keywords">
       <meta content="" name="description">
