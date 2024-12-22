c2faf2461a83b29ff298dc3ca22d1faa7a9242a9
RFIDGMPS <kyebejeanu@gmail.com>
2024-12-22 23:13:33 +0800

    s

--- terms.php
+++ terms.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<!-- <head>
 <?php include 'admin/header.php'; ?>
+<!-- <head>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Terms and Conditions</title>
