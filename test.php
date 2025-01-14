4d2c6d380058b7602e27197c6a92bd3813e9e801
RFIDGMPS <kyebejeanu@gmail.com>
2025-01-14 16:20:52 +0800

    d

--- test.php
+++ test.php
@@ -1,41 +1,37 @@
 <?php
 include 'connection.php';
 
-// SQL query to fetch all records
-$sql = "SELECT * FROM personell_logs";
-$result = $db->query($sql);
+// Array of personnel IDs to choose from
+$personnel_ids = [37, 1172, 6, 5, 4, 3];
 
-// Check if records exist
-if ($result->num_rows > 0) {
-    // Display data in a table
-    echo "<table border='1' cellspacing='0' cellpadding='5'>";
-    echo "<tr>
-            <th>ID</th>
-            <th>Personnel ID</th>
-            <th>Time In (AM)</th>
-            <th>Time Out (AM)</th>
-            <th>Time In (PM)</th>
-            <th>Time Out (PM)</th>
-            <th>Date Logged</th>
-            <th>Location</th>
-          </tr>";
+// Array of possible room names
+$rooms = ['Gate', 'Lab1', 'ComLab1', 'ClassroomA', 'Room102', 'Lab2', 'ComLab2', 'MeetingRoom1', 'Hall1', 'Room201'];
 
-    // Fetch and display rows
-    while ($row = $result->fetch_assoc()) {
-        echo "<tr>
-                <td>" . $row['id'] . "</td>
-                <td>" . $row['personnel_id'] . "</td>
-                <td>" . $row['time_in_am'] . "</td>
-                <td>" . $row['time_out_am'] . "</td>
-                <td>" . $row['time_in_pm'] . "</td>
-                <td>" . $row['time_out_pm'] . "</td>
-                <td>" . $row['date_logged'] . "</td>
-                <td>" . $row['location'] . "</td>
-              </tr>";
+// Loop to insert 30 records
+for ($i = 0; $i < 30; $i++) {
+    // Randomly select a personnel ID
+    $personnel_id = $personnel_ids[array_rand($personnel_ids)];
+
+    // Randomly select a time_in and time_out (just as examples, you can modify this logic)
+    $time_in = date('Y-m-d H:i:s', strtotime("-" . rand(1, 10) . " hours"));
+    $time_out = date('Y-m-d H:i:s', strtotime($time_in . " +" . rand(1, 5) . " hours"));
+
+    // Randomly select a room
+    $location = $rooms[array_rand($rooms)];
+
+    // Get the current date for date_logged
+    $date_logged = date('Y-m-d');
+
+    // SQL query to insert data
+    $sql = "INSERT INTO room_logs (personnel_id, time_in, time_out, date_logged, location) 
+            VALUES ('$personnel_id', '$time_in', '$time_out', '$date_logged', '$location')";
+
+    // Execute the query
+    if ($db->query($sql) === TRUE) {
+        echo "Record $i inserted successfully<br>";
+    } else {
+        echo "Error: " . $sql . "<br>" . $db->error . "<br>";
     }
-    echo "</table>";
-} else {
-    echo "No records found.";
 }
 
 // Close the connection
