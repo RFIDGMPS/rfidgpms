f42fa442e9188cd46795556ee746c61c6f737701
RFIDGMPS <kyebejeanu@gmail.com>
2025-01-14 16:07:13 +0800

    S

--- test.php
+++ test.php
@@ -1,29 +1,40 @@
 <?php
 include 'connection.php';
 
-// List of random room names
-$rooms = ['Gate', 'Lab1', 'ComLab1', 'Library', 'Auditorium', 'AdminOffice'];
 
-// Generate and insert 60 records
+// Personnel IDs to choose from
+$personnel_ids = [37, 1172, 6, 5, 4, 3];
+
+// Locations (rooms)
+$locations = ['Gate', 'Lab1', 'ComLab1', 'Lab2', 'RoomA', 'RoomB', 'RoomC'];
+
+// Insert 60 random records
 for ($i = 0; $i < 60; $i++) {
-    // Generate random data
-    $personnel_id = rand(1, 100); // Assuming personnel IDs range from 1 to 100
-    $date_logged = date('Y-m-d', strtotime("-" . rand(0, 30) . " days")); // Random date within the last 30 days
-    $time_in_am = date('H:i:s', strtotime(rand(7, 8) . ":" . rand(0, 59))); // Random time between 7:00 and 8:59
-    $time_out_am = date('H:i:s', strtotime(rand(11, 12) . ":" . rand(0, 59))); // Random time between 11:00 and 12:59
-    $time_in_pm = date('H:i:s', strtotime(rand(13, 14) . ":" . rand(0, 59))); // Random time between 13:00 and 14:59
-    $time_out_pm = date('H:i:s', strtotime(rand(16, 17) . ":" . rand(0, 59))); // Random time between 16:00 and 17:59
-    $location = $rooms[array_rand($rooms)]; // Random room from the list
+    // Randomly pick personnel_id
+    $personnel_id = $personnel_ids[array_rand($personnel_ids)];
+
+    // Randomly pick location
+    $location = $locations[array_rand($locations)];
+
+    // Generate random times
+    $time_in_am = date("H:i:s", strtotime("08:00:00") + rand(0, 3600)); // Between 8:00 AM and 9:00 AM
+    $time_out_am = date("H:i:s", strtotime($time_in_am) + rand(3600, 7200)); // 1-2 hours later
+
+    $time_in_pm = date("H:i:s", strtotime("13:00:00") + rand(0, 3600)); // Between 1:00 PM and 2:00 PM
+    $time_out_pm = date("H:i:s", strtotime($time_in_pm) + rand(3600, 7200)); // 1-2 hours later
+
+    // Generate random date within the past month
+    $date_logged = date("Y-m-d", strtotime("-" . rand(0, 30) . " days"));
 
     // SQL query to insert data
-    $sql = "INSERT INTO personell_logs (personnel_id, time_in_am, time_out_am, time_in_pm, time_out_pm, date_logged, location)
+    $sql = "INSERT INTO personell_logs (personell_id, time_in_am, time_out_am, time_in_pm, time_out_pm, date_logged, location)
             VALUES ('$personnel_id', '$time_in_am', '$time_out_am', '$time_in_pm', '$time_out_pm', '$date_logged', '$location')";
 
     // Execute the query
-    if ($db->query($sql) === TRUE) {
-        echo "Record $i inserted successfully with Personnel ID: $personnel_id, Location: $location<br>";
+    if ($conn->query($sql) === TRUE) {
+        echo "Record $i inserted successfully.<br>";
     } else {
-        echo "Error on record $i: " . $sql . "<br>" . $db->error . "<br>";
+        echo "Error on record $i: " . $sql . "<br>" . $conn->error . "<br>";
     }
 }
 
