a2b86e3cbedb4aa6bc3e83fd79a847724c45c5c9
RFIDGMPS <kyebejeanu@gmail.com>
2025-01-14 16:10:11 +0800

    d

--- test.php
+++ test.php
@@ -1,41 +1,41 @@
 <?php
 include 'connection.php';
 
+// SQL query to fetch all records
+$sql = "SELECT * FROM personell_logs";
+$result = $conn->query($sql);
 
-// Personnel IDs to choose from
-$personnel_ids = [37, 1172, 6, 5, 4, 3];
+// Check if records exist
+if ($result->num_rows > 0) {
+    // Display data in a table
+    echo "<table border='1' cellspacing='0' cellpadding='5'>";
+    echo "<tr>
+            <th>ID</th>
+            <th>Personnel ID</th>
+            <th>Time In (AM)</th>
+            <th>Time Out (AM)</th>
+            <th>Time In (PM)</th>
+            <th>Time Out (PM)</th>
+            <th>Date Logged</th>
+            <th>Location</th>
+          </tr>";
 
-// Locations (rooms)
-$locations = ['Gate', 'Lab1', 'ComLab1', 'Lab2', 'RoomA', 'RoomB', 'RoomC'];
-
-// Insert 60 random records
-for ($i = 0; $i < 60; $i++) {
-    // Randomly pick personnel_id
-    $personnel_id = $personnel_ids[array_rand($personnel_ids)];
-
-    // Randomly pick location
-    $location = $locations[array_rand($locations)];
-
-    // Generate random times
-    $time_in_am = date("H:i:s", strtotime("08:00:00") + rand(0, 3600)); // Between 8:00 AM and 9:00 AM
-    $time_out_am = date("H:i:s", strtotime($time_in_am) + rand(3600, 7200)); // 1-2 hours later
-
-    $time_in_pm = date("H:i:s", strtotime("13:00:00") + rand(0, 3600)); // Between 1:00 PM and 2:00 PM
-    $time_out_pm = date("H:i:s", strtotime($time_in_pm) + rand(3600, 7200)); // 1-2 hours later
-
-    // Generate random date within the past month
-    $date_logged = date("Y-m-d", strtotime("-" . rand(0, 30) . " days"));
-
-    // SQL query to insert data
-    $sql = "INSERT INTO personell_logs (personnel_id, time_in_am, time_out_am, time_in_pm, time_out_pm, date_logged, location)
-            VALUES ('$personnel_id', '$time_in_am', '$time_out_am', '$time_in_pm', '$time_out_pm', '$date_logged', '$location')";
-
-    // Execute the query
-    if ($db->query($sql) === TRUE) {
-        echo "Record $i inserted successfully.<br>";
-    } else {
-        echo "Error on record $i: " . $sql . "<br>" . $db->error . "<br>";
+    // Fetch and display rows
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>
+                <td>" . $row['id'] . "</td>
+                <td>" . $row['personell_id'] . "</td>
+                <td>" . $row['time_in_am'] . "</td>
+                <td>" . $row['time_out_am'] . "</td>
+                <td>" . $row['time_in_pm'] . "</td>
+                <td>" . $row['time_out_pm'] . "</td>
+                <td>" . $row['date_logged'] . "</td>
+                <td>" . $row['location'] . "</td>
+              </tr>";
     }
+    echo "</table>";
+} else {
+    echo "No records found.";
 }
 
 // Close the connection
